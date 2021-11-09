2b483ae2643e4638c3bba6f0c784ce78f3f0c182
CHETAN GOUR <chetangour88@gmail.com>
2021-11-08 20:47:42 -0800

    company schema updated

--- Backend/Models/CompanySchema.js
+++ Backend/Models/CompanySchema.js
@@ -5,12 +5,64 @@ var companySchema = new mongoose.Schema(
       name: {
         type: String,
         required: true,
+        maxlength: 100,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true,
+        maxlength: 100,
+        trim: true
+      },
+      phone: {
+        type: Number,
+        required: true,
+        min: 0, 
+        max: 10
+      },
+      address: new mongoose.Schema(
+        {
+          street_address: {
+            type: String,
+            required: true,
+            maxlength: 100,
+            trim: true
+          },
+          city: {
+            type: String,
+            required: true,
+            maxlength: 40,
+            trim: true
+          },
+          state: {
+            type: String,
+            required: true,
+            maxlength: 40,
+            trim: true
+          },
+          country: {
+            type: String,
+            required: true,
+            maxlength: 40,
+            trim: true
+          },
+          pincode: {
+            type: Number,
+            required: true,
+            min: 0, 
+            max: 6
+          },
+        }
+      ),
+      description: {
+        type: String,
+        required: true,
         maxlength: 32,
         trim: true
       },
-      description: {
-        type: String,
-        maxlength: 1000,
+      industries: {
+        type: [String],
+        required: true,
       }
     }
 )
