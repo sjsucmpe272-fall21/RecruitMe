df9bdc217a5220706d166bc12803d2d82b5a9fa8
Vineet Karmiani <vineetkarmiani@gmail.com>
2021-11-11 16:12:38 -0800

    Adding icons in header file

---
+++ frontend/src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Header.css"
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import TinderLogo from "./Tinder_logo_PNG18.png"
+import IconButton from '@mui/material/IconButton';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large"/>
+            </IconButton>
+            
+            <img className="header__logo" src={TinderLogo} alt="RecruitMe"/>
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize="large"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
