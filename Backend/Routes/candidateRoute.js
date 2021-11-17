35f7279b4959c7829ffda465fce5f6837d3403bf
Vineet Karmiani <vineetkarmiani@gmail.com>
2021-11-16 18:04:09 -0800

    Added get jobs from linkedin

--- Backend/Routes/candidateRoute.js
+++ Backend/Routes/candidateRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const{apply_job} = require("../Controllers/candidateController")
-const{withdraw_job,getCandidateProfile} = require("../Controllers/candidateController")
+const{withdraw_job,getCandidateProfile,getJobs} = require("../Controllers/candidateController")
 
-
+router.get("/getJobs",getJobs);
 router.post("/apply_job",apply_job);
 router.post("/withdraw_job",withdraw_job);
 router.post("/candidate/profile",getCandidateProfile);
