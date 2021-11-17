f1b7342f33bf46ad088b4c3757253ec895cb3649
Vineet Karmiani <vineetkarmiani@gmail.com>
2021-11-16 18:04:38 -0800

    Added candidate route and recruiter route

---
+++ frontend/src/authHelper/auth.js
@@ -0,0 +1,12 @@
+export const isAuthenticated = () =>{
+    if(typeof window=="undefined")
+    {
+        return false
+    }   
+    if(localStorage.getItem("jwt")){
+        return JSON.parse(localStorage.getItem("jwt"))
+    }
+    else{
+        return false;
+    }
+}
